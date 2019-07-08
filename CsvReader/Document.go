23e00892948d1febc077c6726e096e3654aff04b
Alexander Miles <alexandergmiles@hotmail.com>
2019-07-08 16:34:40 +0100

    Started on processing records

--- CsvReader/Document.go
+++ CsvReader/Document.go
@@ -14,7 +14,7 @@ type Document struct {
 
 //GetRecordFromID will return a pointer to the requested record from the ID passed
 //Pointer returned because we dont know how big these records will actually be
-func (document Document) GetRecordPointerFromID(id string) (*Record, error) {
+ func (document *Document) GetRecordPointerFromID(id string) (*Record, error) {
 	result := document.Records[id]
 	//Panics if a record cant be found
 	if result == (Record{}) {
@@ -26,15 +26,32 @@ func (document Document) GetRecordPointerFromID(id string) (*Record, error) {
 }
 
 //ReadFileIn will read the file in and return the result of the load
-func (document Document) ReadFileIn(path string) bool {
+func (document *Document) GetStringFromPath(path string)  {
 	fileResult, err := getFileString(path)
 	if err != nil {
-		return false
+		panic("AHH")
 	}
 	document.FileDetails = fileResult
-	return true
 }
 
+func (document *Document) ProcessRecordAsGeneric() bool {
+	//Get header row
+
+	//Get the record value
+
+	//Store record in document map
+
+	return false
+}
+
+func (document *Document) ProcessRecordAsType(typeParser func(data string)) bool {
+	//Read each line
+	//Pass each line to function
+
+
+
+	return true
+}
 
 func getFileString(pathToFile string) (string, error) {
 	csv, err := ioutil.ReadFile(pathToFile)
--- main.go
+++ main.go
@@ -12,12 +12,14 @@ func main() {
 	records["FirstRecord"] = CsvReader.Record{Value:"First record"}
 
 	document := CsvReader.Document{Title: "THis is a title", Records: records }
-	document.ReadFileIn("DataAnalysis/test.csv")
 
+	//I dont know if this is how it should be done
+	document.GetStringFromPath("DataAnalysis/test.csv")
 	result, err := document.GetRecordPointerFromID("FirstRecord")
 
 	HandleError(err)
 	fmt.Println(result.Value)
+	fmt.Println(document.FileDetails)
 }
 
 func HandleError(err error) {
